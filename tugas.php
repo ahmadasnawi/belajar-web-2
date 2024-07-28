3327cc403060cbbc02c4c478ebc64a57013e55ea
ahmadasnawi <ahmadaries899@gmail.com>
2024-07-28 12:16:38 +0700

    first commit

---
+++ tugas.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <section>
+        <nav>
+            <div class="left">
+                <input type="text" placeholder="Search courses">
+                <i class="fa-solid fa-magnifying-glass"></i>
+            </div>
+            <div class="mid">
+                <img src="./img/A34BFBDF-1BEB-40E7-9BA5-1AF6E88051C0-removebg-preview.png" alt="">
+            </div>
+            <div class="right">
+                <div class="kiri">
+                <i class="fa-regular fa-heart"></i>
+                <i class="fa-solid fa-bell"></i>
+                <i class="fa-solid fa-bag-shopping"></i>
+                </div>
+                <div class="kanan">
+                <i class="fa-solid fa-user"></i>
+                </div>
+            </div>
+        </nav>
+        <header>
+            <ul>
+                <li>New Arrival</li>
+                <li>Men</li>
+                <li>Women</li>
+                <li>Kids</li>
+                <li>Shoes</li>
+                <li>Accessories</li>
+                <li>Bags</li>
+                <li>Brand</li>
+                <li>Sale</li>
+            </ul>
+        </header>
+        <main>
+            <div class="left-side">
+                <div class="img1">
+                    <img src="./img/clothing-rack-with-floral-hawaiian-shirts-hangers-hat.jpg" alt="">
+                </div>
+                <div class="deskripsi">
+                    <h2>Choose <br>the best</h2>
+                    <p>We believe fashion is all <br>about self expression and <br>embracing your individuality</p>
+                </div>
+
+            </div>
+            <div class="right-side">
+                <div class="title">
+                    <p><b>Clothes</b><i> your <br>sprite </i><b>of Fashion</b></p>
+                </div>
+                <button>Shop Now</button>
+                <div class="img2">
+                    <img src="./img/IMG_2907.jpg" alt="">
+                    <img src="./img/IMG_2909.jpg" alt="">
+                </div>
+            </div>
+        </main>
+    </section>  
+</body>
+</html>
